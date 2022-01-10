3ce356c98051b8fcf7de2308a38cd95be1d63a9f
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-01-10 15:36:26 +0100

    starting product page datas fetch

---
+++ front/js/product.js
@@ -0,0 +1,9 @@
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+const id = urlParams.get("id")
+//console.log({id})
+
+fetch(`http://localhost:3000/api/products/${id}`)
+.then(response => response.json())
+.then(res => console.log(res))
+
